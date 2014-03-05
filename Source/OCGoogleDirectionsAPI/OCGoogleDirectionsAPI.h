e96f016329971ff0078584dde62b422f36431884
marciniwanicki <marciniwanicki.pl@gmail.com>
2014-03-06 00:59:26 +0100

    Adding OCGoogleDirectionsAPI contains all required import directives.

---
+++ Source/OCGoogleDirectionsAPI/OCGoogleDirectionsAPI.h
@@ -0,0 +1,29 @@
+//
+//  OCGoogleDirectionsAPI.h
+//  OCGoogleDirectionsAPI
+//
+//  Created by Marcin Iwanicki on 3/6/14.
+//
+//
+
+#ifndef OCGoogleDirectionsAPI_OCGoogleDirectionsAPI_h
+#define OCGoogleDirectionsAPI_OCGoogleDirectionsAPI_h
+
+#import "OCDirectionsAPIClient.h"
+#import "OCDirectionsBounds.h"
+#import "OCDirectionsCommonTypes.h"
+#import "OCDirectionsDistance.h"
+#import "OCDirectionsDuration.h"
+#import "OCDirectionsLeg.h"
+#import "OCDirectionsPolyline.h"
+#import "OCDirectionsRequest.h"
+#import "OCDirectionsRequestRestriction.h"
+#import "OCDirectionsRequestTravelMode.h"
+#import "OCDirectionsRequestUnit.h"
+#import "OCDirectionsResponse.h"
+#import "OCDirectionsResponseVehicleType.h"
+#import "OCDirectionsRoute.h"
+#import "OCDirectionsStep.h"
+#import "OCDirectionsWaypoint.h"
+
+#endif
